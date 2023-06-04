fb5a492597de6c827b81f0291769c86e16e1fcba
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-06-04 09:43:33 -0600

    create Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav>
+    <h1>{{ title }}</h1>
+  </nav>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  name: "Navbar",
+  props: {
+    title: String
+  }
+};
+</script>
+
+
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: blue;
+  color: antiquewhite;
+  height: 120px;
+  width: 100%;
+  position: fixed;
+}
+</style>
