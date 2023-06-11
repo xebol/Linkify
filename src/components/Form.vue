ea14d63172871bf19b5283ba0cfce7166506eb8a
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-06-11 15:57:45 -0600

    add new component, Form

---
+++ src/components/Form.vue
@@ -0,0 +1,25 @@
+<template>
+  <form class="login-form">
+    <div class="login">
+      <label>Email</label>
+      <input type="email" name="email" placeholder="Email" />
+      <label>Password</label>
+      <input type="password" name="password">
+    </div>
+  </form>
+</template>
+
+
+<script>
+export default {
+  name: "Form",
+};
+</script>
+
+
+<style scoped>
+  .login-form {
+    display: flex;
+    height: 130vh;
+  }
+</style>
