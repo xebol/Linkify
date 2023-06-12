a18381a629054c50ae515f0fdfe09c815fbccbfd
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-06-12 17:08:29 -0600

    add login button and register button to Form component

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,12 +1,18 @@
 <template>
-    <form class="login-form">
-      <div class="login">
-        <label>Email</label>
-        <input type="email" name="email" />
-        <label>Password</label>
-        <input type="password" name="password">
+  <form class="login-form">
+    <div class="login">
+      <h2>Login</h2>
+      <label>Email</label>
+      <input type="email" name="email" />
+      <label>Password</label>
+      <input type="password" name="password">
+      <button type="submit">Login</button>
+      <div class="register">
+        <p>Don't have an account?</p>
+        <button type="submit">Register Here</button>
       </div>
-    </form>
+    </div>
+  </form>
 </template>
 
 
@@ -23,17 +29,29 @@ export default {
   margin: 0 auto;
   padding-top: 225px;
   border-radius: 4px;
+  align-items: center;
+  display: flex;
+
 }
 
 .login {
   display: flex;
   flex-direction: column;
   margin-bottom: 15px;
-  background-color:rgba(8, 138, 104, 0.301);
+  background-color: rgba(8, 138, 104, 0.301);
   padding: 20px;
   font-size: 1.5em;
   width: 100%;
   border-radius: 4px;
+}
+
+.login h2 {
+  padding-bottom: 15px;
+}
+
+.register p {
+  padding-bottom: 5px;
+  font-size: 1em;
 }
 
 label {
@@ -50,15 +68,19 @@ input[type="password"] {
 }
 
 button[type="submit"] {
+  width: 150px;
   padding: 10px;
-  background-color: #4caf50;
+  padding-top: 10px;
+  margin-bottom: 20px;
+  background-color: #0e3b0f;
   color: #fff;
   border: none;
-  border-radius: 4px;
+  border-radius: 25px;
   cursor: pointer;
+  font-size: medium;
 }
 
 button[type="submit"]:hover {
-  background-color: #45a049;
+  background-color: #184f1b;
 }
 </style>
