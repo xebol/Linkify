baaaa8b2ba1fe1d20b24d1f846a18e3d162e2959
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-06-14 14:02:47 -0600

    add Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="onClick()" v-bind:style="{background: color}" class="btn">{{ text }}</button>
+</template>
+
+
+<script>
+  export default {
+    name: "Button",
+    props: {
+      text: String,
+      color: String
+    },
+    methods: {
+      //adding a method when an event is defined
+      onClick() {
+        this.$emit('btn-click')
+      }
+    }
+  }
+</script>
