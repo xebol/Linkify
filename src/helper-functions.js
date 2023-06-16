a02c979ce43760bd79ab545a02744da65f2f60fe
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-06-15 19:27:37 -0600

    create a file for helper functions

---
+++ src/helper-functions.js
@@ -0,0 +1,14 @@
+const generateRandomString = function() {
+  const string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let uniqueID = '';
+  for (let i = 0; i < 6; i++) {
+    uniqueID += string.charAt(Math.floor(Math.random() * string.length));
+  }
+  return uniqueID;
+};
+
+
+
+module.exports = {
+  generateRandomString
+}
