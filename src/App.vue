ffb240d7bbf2db9d86d4153cb8d62f42ab4ad7a8
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-06-24 15:38:50 -0600

    remove all the methods and the data from App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,79 +9,72 @@
 <script>
 import Navbar from "./components/Navbar.vue";
 import Footer from "./components/Footer.vue";
-import Form from "./components/Form.vue";
-import MyURLs from "./components/MyURLs.vue";
-import AddURL from "./components/AddURL.vue";
-import NewURL from "./components/NewURL.vue";
 
 export default
   {
     name: "App",
     components: {
       Navbar,
-      Form,
-      MyURLs,
-      AddURL,
-      NewURL,
       Footer
     },
-    data() {
-      return {
-        urls: [],
-        addNewUrl: 'MyURLs',
-        urlInput: '',
-        newShortUrl: ''
-      };
-    },
-    methods: {
-      addUrl() {
-        this.addNewUrl = 'AddURL';
-      },
-      async submitUrl(url) {
-        const res = await fetch('api/urls', {
-          method: 'POST',
-          headers: {
-            'Content-type': 'application/json',
-          },
-          body: JSON.stringify(url)
-        });
+  }
+    // data() {
+    //   return {
+    //     urls: [],
+    //     addNewUrl: 'MyURLs',
+    //     urlInput: '',
+    //     newShortUrl: ''
+    //   };
+    // },
+  //   methods: {
+  //     addUrl() {
+  //       this.addNewUrl = 'AddURL';
+  //     },
+  //     async submitUrl(url) {
+  //       const res = await fetch('api/urls', {
+  //         method: 'POST',
+  //         headers: {
+  //           'Content-type': 'application/json',
+  //         },
+  //         body: JSON.stringify(url)
+  //       });
 
-        const data = await res.json();
+  //       const data = await res.json();
 
-        //redirect to NewURL component instead of MyURLs component
-        this.addNewUrl = 'NewURL';
-        this.urls = [...this.urls, data];
-        this.urlInput = url.longURL;
-        this.newShortUrl = url.shortURL;
-      },
-      async deleteUrl(id) {
-        const res = await fetch(`api/urls/${id}`, {
-          method: 'DELETE'
-        });
+  //       //redirect to NewURL component instead of MyURLs component
+  //       this.addNewUrl = 'NewURL';
+  //       this.urls = [...this.urls, data];
+  //       this.urlInput = url.longURL;
+  //       this.newShortUrl = url.shortURL;
+  //     },
+  //     async deleteUrl(id) {
+  //       const res = await fetch(`api/urls/${id}`, {
+  //         method: 'DELETE'
+  //       });
 
-        res.status === 200 ? (this.urls = this.urls.filter((url) => url.id !== id)) : alert("Error deleting task");
-      },
-      async fetchURLs() {
-        const res = await fetch('api/urls');
+  //       res.status === 200 ? (this.urls = this.urls.filter((url) => url.id !== id)) : alert("Error deleting task");
+  //     },
+  //     async fetchURLs() {
+  //       const res = await fetch('api/urls');
 
-        const data = await res.json();
+  //       const data = await res.json();
 
-        return data;
+  //       return data;
 
-      },
-      navBarLinks(componentName) {
-        console.log("I clicked the NavBar")
-        if (componentName === 'AddURL') {
-          this.addNewUrl = 'AddURL';
-        } else if (componentName === 'MyURLs') {
-          this.addNewUrl = 'MyURLs';
-        }
-      },
-    },
-    async created() {
-      this.urls = await this.fetchURLs();
-    },
-  };
+  //     },
+  //     navBarLinks(componentName) {
+  //       console.log("I clicked the NavBar")
+  //       if (componentName === 'AddURL') {
+  //         this.addNewUrl = 'AddURL';
+  //       } else if (componentName === 'MyURLs') {
+  //         this.addNewUrl = 'MyURLs';
+  //       }
+  //     },
+  //   },
+  //   async created() {
+  //     this.urls = await this.fetchURLs();
+  //   },
+  // };
 </script>
 
 <style>
