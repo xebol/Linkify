31ca30c95ea7e0ed23a1c8e8500144e5f4c56bf5
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-06-25 16:18:56 -0600

    add basic styling to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,9 @@
 <template>
-  <div>
+  <div class="about">
     <Navbar title="ABOUT" />
     <p>Welcome to Linkify. This app allows you to convert long URLs into shorter ones, making them easier to share and remember.</p>
-    <p>Features:</p>
-    <ul>
-      <li>Shorten any long URL to a compact form</li>
-    </ul>
+    <p class="features">Features:</p>
+      <p>Shorten any long URL to a compact form</p>
     <p>Get started now and simplify your links!</p>
   </div>
 </template>
@@ -20,3 +18,10 @@ export default {
   }
 };
 </script>
+
+<style>
+.about {
+  text-align: center;
+  padding: 20px;
+}
+</style>
